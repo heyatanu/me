21030becc1ae033dd92390c33561ee98987d7b86
Atanu <atanud662@gmail.com>
2021-01-11 12:37:17 +0530

    BTS Update

--- js/Main.js
+++ js/Main.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
     let $btns=$('.work_section .container .button_group button');
     
     $btns.click(function(e){
-        $('.work_section .container .button_group button').removeClass('btn-danger');
-        e.target.classList.add("btn-danger");
+        $('.work_section .container .button_group button').removeClass('active_filter_btn');
+        e.target.classList.add("active_filter_btn");
         // console.log( e.target)
         let selector= $(e.target).attr("data-filter");
         $('.work_section .row').isotope({
