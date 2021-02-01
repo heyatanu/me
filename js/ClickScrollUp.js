830b9e1ec107f30ec92d4d8c1e815be94012bb06
Atanu <atanud662@gmail.com>
2021-02-01 15:53:33 +0530

    Work for xm update

--- js/ClickScrollUp.js
+++ js/ClickScrollUp.js
@@ -5,6 +5,8 @@ var btn = $('#onclickgoup');
 $(window).scroll(function() {
   if ($(window).scrollTop() > 150) {
     btn.addClass('show');
+
+    
   } else {
     btn.removeClass('show');
   }
@@ -15,3 +17,26 @@ btn.on('click', function(e) {
   $('html, body').animate({scrollTop:0}, '300');
 });
 
+
+
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() < 650) {
+    homefun()
+  }
+  if ($(window).scrollTop() >= 650) {
+    aboutfun()
+  }
+  if ($(window).scrollTop() >= 1350) {
+    skillfun()
+  }
+  if ($(window).scrollTop() >= 2650) {
+    projectfun()
+  }
+
+  if ($(window).scrollTop() > 3350) {
+    workfun()
+  }
+
+
+});
